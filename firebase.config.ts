a4bfc0c3634b5be4cb443d6988a57545c151785d
basyir <basyir-almex@gmail.com>
2025-06-13 10:18:26 +0800

    Protect secrets.

--- firebase.config.ts
+++ firebase.config.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6eWlMRU1SiBB67BEjFcwzzlI39R1G_LU",
-  authDomain: "sheers-website-2025.firebaseapp.com",
-  projectId: "sheers-website-2025",
-  storageBucket: "sheers-website-2025.firebasestorage.app",
-  messagingSenderId: "757424985699",
-  appId: "1:757424985699:web:373bafb2a61c089b184a56"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY!,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
