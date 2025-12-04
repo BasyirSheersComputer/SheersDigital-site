6553ada3a6deed7155a113f1b9cd302cdfbbc258
basyir <basyir-almex@gmail.com>
2025-12-04 16:54:31 +0800

    Fix linting errors: move Particle class outside component and update ESLint config for motion imports

--- src/components/effects/AnimatedBackground.jsx
+++ src/components/effects/AnimatedBackground.jsx
@@ -1,6 +1,48 @@
 import React, { useEffect, useRef } from 'react';
 import './AnimatedBackground.css';
 
+// Particle class - moved outside component to avoid inline class declaration warning
+class Particle {
+    constructor(canvas) {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 3 + 1;
+        this.speedX = Math.random() * 0.5 - 0.25;
+        this.speedY = Math.random() * 0.5 - 0.25;
+        this.opacity = Math.random() * 0.5 + 0.2;
+
+        // Random neon color
+        const colors = [
+            '0, 255, 255',    // cyan
+            '255, 0, 255',    // magenta
+            '124, 252, 0'     // green
+        ];
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+        this.canvas = canvas;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Wrap around screen
+        if (this.x > this.canvas.width) this.x = 0;
+        if (this.x < 0) this.x = this.canvas.width;
+        if (this.y > this.canvas.height) this.y = 0;
+        if (this.y < 0) this.y = this.canvas.height;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`;
+        ctx.shadowBlur = 15;
+        ctx.shadowColor = `rgba(${this.color}, ${this.opacity})`;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.shadowBlur = 0;
+    }
+}
+
 const AnimatedBackground = () => {
     const canvasRef = useRef(null);
 
@@ -20,53 +62,12 @@ const AnimatedBackground = () => {
         resizeCanvas();
         window.addEventListener('resize', resizeCanvas);
 
-        // Particle class
-        class Particle {
-            constructor() {
-                this.x = Math.random() * canvas.width;
-                this.y = Math.random() * canvas.height;
-                this.size = Math.random() * 3 + 1;
-                this.speedX = Math.random() * 0.5 - 0.25;
-                this.speedY = Math.random() * 0.5 - 0.25;
-                this.opacity = Math.random() * 0.5 + 0.2;
-
-                // Random neon color
-                const colors = [
-                    '0, 255, 255',    // cyan
-                    '255, 0, 255',    // magenta
-                    '124, 252, 0'     // green
-                ];
-                this.color = colors[Math.floor(Math.random() * colors.length)];
-            }
-
-            update() {
-                this.x += this.speedX;
-                this.y += this.speedY;
-
-                // Wrap around screen
-                if (this.x > canvas.width) this.x = 0;
-                if (this.x < 0) this.x = canvas.width;
-                if (this.y > canvas.height) this.y = 0;
-                if (this.y < 0) this.y = canvas.height;
-            }
-
-            draw() {
-                ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`;
-                ctx.shadowBlur = 15;
-                ctx.shadowColor = `rgba(${this.color}, ${this.opacity})`;
-                ctx.beginPath();
-                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-                ctx.fill();
-                ctx.shadowBlur = 0;
-            }
-        }
-
         // Create particles
         const createParticles = () => {
             const particleCount = Math.min(100, Math.floor(window.innerWidth / 15));
             particles = [];
             for (let i = 0; i < particleCount; i++) {
-                particles.push(new Particle());
+                particles.push(new Particle(canvas));
             }
         };
         createParticles();
@@ -78,7 +79,7 @@ const AnimatedBackground = () => {
             // Draw connections
             particles.forEach((particle, index) => {
                 particle.update();
-                particle.draw();
+                particle.draw(ctx);
 
                 // Draw lines between nearby particles
                 for (let j = index + 1; j < particles.length; j++) {
